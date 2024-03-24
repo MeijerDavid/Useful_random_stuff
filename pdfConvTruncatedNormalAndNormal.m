c43c2319806ef3f5d62ad7e4ec22edf884901936
MeijerDavid <58221670+MeijerDavid@users.noreply.github.com>
2024-03-24 21:43:30 +0100

    Significant simplification

--- pdfConvTruncatedNormalAndNormal.m
+++ pdfConvTruncatedNormalAndNormal.m
@@ -1,29 +1,29 @@
 function y = pdfConvTruncatedNormalAndNormal(x,a_TN,b_TN,mu_TN,sd_TN,sd_N,log_flag)
 %Computes the (log-) pdf at x for a convolution of a truncated normal 
 %[parameters: a_TN,b_TN,mu_TN,sd_TN] and normal distribution (mu_N=0,sd_N). 
+%
+%David Meijer, 04-03-2024
 
 if nargin < 7
     log_flag = false;
 end
 
-%The formula is adopted from Turban 2010: 
+%I originally adopted the formula from Turban 2010: 
 %http://www.columbia.edu/~st2511/notes/Convolution%20of%20truncated%20normal%20and%20normal.pdf
+%But my computational implementation is now quite different  
+
 var_TN = sd_TN.^2;
 var_N = sd_N.^2;
-alpha = var_N.*(x-mu_TN)./(var_TN+var_N);
-beta = sqrt((var_TN.*var_N)./(var_TN+var_N));
+
+weight_TN = var_N./(var_TN+var_N);      %Posterior parameters (from product of N and TN)
+mu_post = x + weight_TN.*(mu_TN-x);
+sd_post = sqrt(weight_TN.*var_TN);
 
 if log_flag
     %Compute y=log(pdf)
-    %log_gamma = log(sqrt(2*pi)*beta) - log(2*pi*sd_N.*sd_TN) - log(normcdf_quick((mu_TN-b_TN)./sd_TN, (mu_TN-a_TN)./sd_TN));
-    %y = log_gamma - (x-mu_TN).^2./(2*(var_TN+var_N)) + log(normcdf_quick((x-b_TN-alpha)./beta, (x-a_TN-alpha)./beta));
-    y = log(sqrt(2*pi)*beta) - log(2*pi*sd_N.*sd_TN) - log(normcdf_quick((mu_TN-b_TN)./sd_TN, (mu_TN-a_TN)./sd_TN)) - (x-mu_TN).^2./(2*(var_TN+var_N)) + log(normcdf_quick((x-b_TN-alpha)./beta, (x-a_TN-alpha)./beta));
+    y = normlogpdf(x,mu_TN,sqrt(var_TN+var_N)) + normcdf_diff((a_TN-mu_post)./sd_post,(b_TN-mu_post)./sd_post,true) - normcdf_diff((a_TN-mu_TN)./sd_TN,(b_TN-mu_TN)./sd_TN,true);
 else
-    %gamma = sqrt(2*pi)*beta./(2*pi*sd_N.*sd_TN.*(normcdf_quick((mu_TN-a_TN)./sd_TN)-normcdf_quick((mu_TN-b_TN)./sd_TN)));
-    %y = gamma.*exp(-(x-mu_TN).^2./(2*(var_TN+var_N))).*(normcdf_quick((x-a_TN-alpha)./beta)-normcdf_quick((x-b_TN-alpha)./beta));
-    %gamma = sqrt(2*pi)*beta./(2*pi*sd_N.*sd_TN.*normcdf_quick((mu_TN-b_TN)./sd_TN, (mu_TN-a_TN)./sd_TN));
-    %y = gamma.*exp(-(x-mu_TN).^2./(2*(var_TN+var_N))).*normcdf_quick((x-b_TN-alpha)./beta, (x-a_TN-alpha)./beta);
-    y = (sqrt(2*pi)*beta./(2*pi*sd_N.*sd_TN.*normcdf_quick((mu_TN-b_TN)./sd_TN, (mu_TN-a_TN)./sd_TN))) .* exp(-(x-mu_TN).^2./(2*(var_TN+var_N))).*normcdf_quick((x-b_TN-alpha)./beta, (x-a_TN-alpha)./beta);
+    y = normpdf(x,mu_TN,sqrt(var_TN+var_N)) .* normcdf_diff((a_TN-mu_post)./sd_post,(b_TN-mu_post)./sd_post) ./ normcdf_diff((a_TN-mu_TN)./sd_TN,(b_TN-mu_TN)./sd_TN);
 end
 
 end %[EoF]
