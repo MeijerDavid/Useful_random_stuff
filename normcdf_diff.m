b3685cd09af04f59bc1a25b7f6cd317a4f4fbfb3
MeijerDavid <58221670+MeijerDavid@users.noreply.github.com>
2024-03-24 21:44:10 +0100

    Implemented working version of log normcdf difference

--- normcdf_diff.m
+++ normcdf_diff.m
@@ -1,4 +1,4 @@
-function p = normcdf_diff(z1,z2)
+function p = normcdf_diff(z1,z2,log_flag)
 %Precisely compute p = normcdf(z2)-normcdf(z1), avoids false p=0 returns.
 %With (implicit) expansion support, but slower because it's explicit.
 %
@@ -8,98 +8,79 @@ function p = normcdf_diff(z1,z2)
 %Implementation with care for numerical stability.
 %Adapted from: https://github.com/cossio/TruncatedNormal.jl
 %
-%David Meijer, 28-6-2023
+%However, since normcdf(-40) = 0 due to the "realmin", this method can 
+%still lead to inaccuracies. To prevent those, it is better to work with
+%logarithm transformed probabilities. Set "log_flag" to true to compute the
+%log of the probability difference: i.e. lp = log(normcdf(z2)-normcdf(z1)).
+%Retrieve the probability difference with p=exp(lp). Or, if there are cases
+%where z1>z2, then use p=real(exp(lp)), to safely get rid of the complex 
+%numbers.
+%
+%David Meijer, 21-3-2024
 
-x = -z1/sqrt(2);
-y = -z2/sqrt(2);
-
-%Apply explicit expansion (see helper function below)
-[x,y] = explicitExpansion(x,y);
-
-%initialize output
-p = nan(size(x));
-
-%first condition
-i1 = x > y;
-if any(i1,'all')
-    %Switch x and y, then multiply output by -1
-    %Avoid indexing because it can be slow
-    x_copy = x;
-    x = ~i1.*x + i1.*y;
-    y = ~i1.*y + i1.*x_copy;
-end
-i1_mask = 2*double(~i1)-1;    %-1 for i1, 1 for ~i1
-
-%second condition
-i2 = abs(x) > abs(y);
-if any(i2,'all')
-    %Multiply x and y by -1, then multiply output by -1
-    %Avoid indexing because it can be slow
-    x = ~i2.*x - i2.*x;
-    y = ~i2.*y - i2.*y;
-end
-i2_mask = 2*double(~i2)-1;    %-1 for i2, 1 for ~i2
-
-%Use erf or erfc for stability
-i3 = (x < 0) & (0 <= y);
-%Fastest to use indexing and vectorization here
-p(i3) = .5 * (erf(x(i3)) - erf(y(i3)));
-p(~i3) = .5 * (erfc(y(~i3)) - erfc(x(~i3)));
-
-%Apply corrections for first and second conditions
-p = p .* i1_mask .* i2_mask;
-
-end %[EoF]
-
-
-%%%%%%%%%%%%%%%%%%%%%%%
-%%% Helper function %%%
-%%%%%%%%%%%%%%%%%%%%%%%
-
-%Replicate input arrays so that their sizes match (c.f. implicit expansion)
-function varargout = explicitExpansion(varargin)
-
-if nargin == 1
-    %Special case
-    varargout{1} = varargin{1};
-    return;
-else
-    %Initialize output    
-    n_var = nargin;
-    varargout = cell(1,n_var);
+if nargin < 3
+    log_flag = false;
 end
 
-%Determine maximum number of dimensions in input arrays
-num_dims = nan(n_var,1);
-for j=1:n_var
-    num_dims(j) = numel(size(varargin{j}));
-end
-max_dims = max(2,max(num_dims));
-
-%Determine size of input arrays
-array_sizes = ones(n_var,max_dims);
-for j=1:n_var
-    array_sizes(j,1:num_dims(j)) = size(varargin{j});
-end
-
-%Determine the maximum size in each dimension across input
-max_size = repmat(max(array_sizes),[n_var 1]);
-
-%Check whether input sizes either match the maximum size or are 1
-i_max = (array_sizes == max_size);
-i_one = (array_sizes == 1);
-assert(all(i_max | i_one,'all'), 'Input size issues');
-
-%Expand the matrices
-for j=1:n_var
-    expand_vector = nan(1,max_dims);
-    expand_vector(i_max(j,:)) = ones(1,sum(i_max(j,:)));
-    expand_vector(i_one(j,:)) = max_size(j,i_one(j,:));
-    varargout{j} = repmat(varargin{j},expand_vector);
-end
+%Work with log transformed probabilities?
+if log_flag
     
-end %[EoF]
+    %Apply explicit expansion (make inputs the same size)
+    x = z1.*ones(size(z2));
+    y = z2.*ones(size(z1));
+    
+    %Compute log(normcdf(z1)) and log(normcdf(z2)) 
+    log_px = nan(size(x));
+    log_py = nan(size(x));
+    i_flip = max(x,y)>0;                                                   
+    log_px(~i_flip) = normlogcdf(x(~i_flip));
+    log_py(~i_flip) = normlogcdf(y(~i_flip));
+    log_py(i_flip) = normlogcdf(-x(i_flip));
+    log_px(i_flip) = normlogcdf(-y(i_flip));
+    
+    %Compute log(normcdf(z2)-normcdf(z1))
+    p = logminexp(log_py,log_px);   
 
+%Work without log transformations (default)    
+else
+    
+    %Apply explicit expansion (make inputs the same size) and convert inputs   
+    x = -z1.*ones(size(z2))/sqrt(2);
+    y = -z2.*ones(size(z1))/sqrt(2);
+    
+    %First condition
+    i1 = x > y;
+    if any(i1,'all')
+        %Switch x and y, then multiply output by -1
+        %Avoid indexing because it can be slow
+        x_copy = x;
+        x = ~i1.*x + i1.*y;
+        y = ~i1.*y + i1.*x_copy;
+    end
+    i1_mask = 2*double(~i1)-1;    %-1 for i1, 1 for ~i1
+
+    %Second condition
+    i2 = abs(x) > abs(y);
+    if any(i2,'all')
+        %Multiply x and y by -1, then multiply output by -1
+        %Avoid indexing because it can be slow
+        x = ~i2.*x - i2.*x;
+        y = ~i2.*y - i2.*y;
+    end
+    i2_mask = 2*double(~i2)-1;    %-1 for i2, 1 for ~i2
+
+    %Use erf or erfc for stability
+    i3 = (x < 0) & (0 <= y);
+    %Fastest to use indexing and vectorization here
+    p = nan(size(x));
+    p(i3) = .5 * (erf(x(i3)) - erf(y(i3)));
+    p(~i3) = .5 * (erfc(y(~i3)) - erfc(x(~i3)));
+
+    %Apply corrections for first and second conditions
+    p = p .* i1_mask .* i2_mask;
+end
+
+end %[EoF]
 
 %%%%%%%%%%%%%%%%%%%%
 %%% Testing code %%%
@@ -109,6 +90,9 @@ end %[EoF]
 % z2 = 100 * randn([100 100 1]);
 % tic; p1 = normcdf(z2)-normcdf(z1); toc
 % tic; p2 = normcdf_diff(z1,z2); toc
-% num_diffs = sum(p1 ~= p2,'all')
-% p1_tmp = p1(:,1,1); p2_tmp = p2(:,1,1); 
-% p3 = [p1_tmp, p2_tmp, p1_tmp~=p2_tmp];    %Have a look at p3
+% num_diffs = sum(abs(p1-p2) > 1e-15,'all')
+% max_diffs = max(abs(p1-p2),[],'all')
+%
+% tic; p3 = real(exp(normcdf_diff(z1,z2,true))); toc
+% num_diffs = sum(abs(p1-p3) > 1e-15,'all')
+% max_diffs = max(abs(p1-p3),[],'all')
